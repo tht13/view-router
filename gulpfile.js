d1c0774399e7ecf53b6b7e2987e94f5b4ec92345
Thomas Townsend <thomas.haakon@gmail.com>
2016-12-04 22:23:23 +0000

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+"use strict";
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const del = require("del");
+const sourcemaps = require("gulp-sourcemaps");
+const path = require("path");
+// const tslint = require("gulp-tslint");
+
+
+const out = "./dist";
+const src = ["./src/**/*.ts", "./src/**/*.d.ts", "./src/**/*.js"];
+
+gulp.task("clean", () => {
+    del(out);
+});
+
+let tsProject = ts.createProject("tsconfig.json");
+
+
+gulp.task("ts-compile", ["clean"], function () {
+    var tsResult = gulp.src(src)
+        .pipe(sourcemaps.init())
+        .pipe(tsProject(ts.reporter.longReporter()))
+        .pipe(sourcemaps.write({
+            includeContent: false,
+            sourceRoot: "./src"
+        }))
+        .pipe(gulp.dest(out));
+    return tsResult;
+});
+
+gulp.task("compile", ["ts-compile"]);
+
+// gulp.task("tslint", () =>
+//     gulp.src(src)
+//         .pipe(tslint())
+//         .pipe(tslint.report("verbose"))
+// );
+
+// gulp.task("abstract_build", ["tslint"], () => {
+//     return gulp.src(sources)
+//         .pipe(compilation())
+//         .pipe(gulp.dest(output));
+// });
+
+// gulp.task("build", ["clean", "tslint"], () => {
+//     return gulp.src(sources)
+//         .pipe(compilation())
+//         .pipe(gulp.dest(output));
+// });
+
+// gulp.task("watch", ["abstract_build"], () => {
+//     gulp.watch(sources, ["abstract_build"]);
+// });
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import * as express from "express";
+import * as assert from "assert";
+import { isNil } from "lodash";
+
+export interface IView {
+  checkRouteValid?(): Promise<boolean>;
+  getContext(): Promise<{}>;
+  setBasicContext(ctx: {}): void;
+}
+
+export interface IViewConstructor {
+  new (req: express.Request, res: express.Response): IView;
+}
+
+interface IViewConfig {
+  id: string;
+  urlPath: string | string[];
+  layout?: string;
+  viewHandlerPath?: string;
+}
+
+interface IViewRouterOptions {
+  basicContectGenerator?: (req?: express.Request, res?: express.Response) => any;
+}
+
+class ViewRouter {
+  private views: Map<string, IViewConfig> = new Map();
+  constructor(views: IViewConfig[], private options: IViewRouterOptions = {}) {
+    for (const view of views) {
+      if (view.urlPath.constructor === Array) {
+        for (const path of view.urlPath) {
+          assert.equal(this.views.has(path), false);
+          this.views.set(path, view);
+        }
+      } else {
+        assert.equal(this.views.has(view.urlPath as string), false);
+        this.views.set(view.urlPath as string, view);
+      }
+    }
+  }
+
+  public async handle(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const view = this.views.get(req.path);
+    await this.getView(req, res, view);
+    next();
+  }
+  private async getView(req: express.Request, res: express.Response, viewConfig: IViewConfig): Promise<void> {
+    let viewConstructor: IViewConstructor = null;
+    if (!isNil(viewConfig.viewHandlerPath)) {
+      viewConstructor = require(viewConfig.viewHandlerPath).default;
+    }
+    const view: IView = (isNil(viewConstructor)) ? this.getDefaultView(req, res) : new viewConstructor(req, res);
+    if (!isNil(view.checkRouteValid) && await !view.checkRouteValid()) {
+      res.end();
+      return;
+    }
+    if (!isNil(this.options.basicContectGenerator)) {
+      view.setBasicContext(this.options.basicContectGenerator(req, res));
+    }
+    view.getContext().then(v =>
+      res.render((isNil(viewConfig.layout)) ? viewConfig.id : viewConfig.layout, v)
+    );
+  }
+  private getDefaultView(req: express.Request, res: express.Response): IView {
+    return {
+      getContext: () => Promise.resolve(
+        (isNil(this.options.basicContectGenerator)) ? void 0 : this.options.basicContectGenerator(req, res)
+      ),
+      setBasicContext: ctx => void 0,
+      checkRouteValid: () => Promise.resolve(true)
+    };
+  }
+}
+
+export function viewRouter(views: IViewConfig[], options?: IViewRouterOptions) {
+  const vr = new ViewRouter(views, options);
+  return (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    vr.handle(req, res, next);
+  };
+}
+
+export default viewRouter;
