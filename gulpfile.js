d326ded928fb1846046fd3aefdbb59c990cc553c
Thomas Townsend <thomas.haakon@gmail.com>
2016-12-10 20:09:19 +0100

    Add tslint to gulp checks

--- gulpfile.js
+++ gulpfile.js
@@ -4,51 +4,38 @@ const ts = require("gulp-typescript");
 const del = require("del");
 const sourcemaps = require("gulp-sourcemaps");
 const path = require("path");
-// const tslint = require("gulp-tslint");
+const tslint = require("gulp-tslint");
 
 
 const out = "./dist";
 const src = ["./src/**/*.ts", "./src/**/*.d.ts", "./src/**/*.js"];
 
-gulp.task("clean", () => {
-    return del(out);
-});
+gulp.task("clean", () =>
+    del(out)
+);
 
 let tsProject = ts.createProject("tsconfig.json");
 
 
-gulp.task("ts-compile", ["clean"], function () {
-    var tsResult = gulp.src(src)
+gulp.task("ts-compile", ["clean"], () =>
+    gulp.src(src)
         .pipe(sourcemaps.init())
         .pipe(tsProject(ts.reporter.longReporter()))
         .pipe(sourcemaps.write({
             includeContent: false,
             sourceRoot: "./src"
         }))
-        .pipe(gulp.dest(out));
-    return tsResult;
-});
+        .pipe(gulp.dest(out))
+);
 
 gulp.task("compile", ["ts-compile"]);
 
-// gulp.task("tslint", () =>
-//     gulp.src(src)
-//         .pipe(tslint())
-//         .pipe(tslint.report("verbose"))
-// );
+gulp.task("tslint", () =>
+    gulp.src(src)
+        .pipe(tslint({
+            formatter: "verbose"
+        }))
+        .pipe(tslint.report())
+);
 
-// gulp.task("abstract_build", ["tslint"], () => {
-//     return gulp.src(sources)
-//         .pipe(compilation())
-//         .pipe(gulp.dest(output));
-// });
-
-// gulp.task("build", ["clean", "tslint"], () => {
-//     return gulp.src(sources)
-//         .pipe(compilation())
-//         .pipe(gulp.dest(output));
-// });
-
-// gulp.task("watch", ["abstract_build"], () => {
-//     gulp.watch(sources, ["abstract_build"]);
-// });
+gulp.task("build", ["tslint", "compile"]);
